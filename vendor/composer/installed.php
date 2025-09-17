7e58f8a4a9ded9aa2339c194066dae95173cb06c
AcedilloShaneChristia <acedilloshanechristia@gmail.com>
2025-09-17 19:36:51 +0800

    Firt commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '25a6819ec682f4b07f165e8fe391a40ea497ef1b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '25a6819ec682f4b07f165e8fe391a40ea497ef1b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
