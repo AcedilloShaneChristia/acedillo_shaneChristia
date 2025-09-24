387888b4192ff4358568be12f071cbded1595b08
AcedilloShaneChristia <acedilloshanechristia@gmail.com>
2025-09-25 00:17:01 +0800

    First commit

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -1,68 +1,39 @@
 <?php
-defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+    defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
-class UserModel extends Model {
-    
-    protected $table = 'users';
-    protected $primary_key = 'id';
-    
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
-     * Pagination + Search
-     */
-    public function page($q = '', $records_per_page = null, $page = null) {
-        if (is_null($page)) {
-            // Return all rows if no pagination
-            return [
-                'records'    => $this->db->table($this->table)->get_all(),
-                'total_rows' => $this->db->table($this->table)->count_all()
-            ];
-        } else {
-            $query = $this->db->table($this->table);
-
-            // If search query is provided
-            if (!empty($q)) {
-                $query->like('id', $q)
-                      ->or_like('username', $q)
-                      ->or_like('email', $q);
-            }
-
-            // Clone before applying pagination to count total
-            $countQuery = clone $query;
-            $data['total_rows'] = $countQuery->select_count('*', 'count')->get()['count'];
-
-            // Fetch paginated records
-            $data['records'] = $query->pagination($records_per_page, $page)
-                                     ->get_all();
-
-            return $data;
+    class UserModel extends Model {
+        
+        protected $table = 'users';
+        protected $primary_key = 'id';
+        
+        public function __construct()
+        {
+            parent::__construct();
         }
-    }
 
-    /**
-     * CRUD helpers
-     */
-    public function all() {
-        return $this->db->table($this->table)->get_all();
-    }
+        public function page($q, $records_per_page = null, $page = null) {
+            if (is_null($page)) {
+                return $this->db->table('users')->get_all();
+            } else {
+                $query = $this->db->table('users');
+                
+                // Build LIKE conditions
+                $query->like('id', '%'.$q.'%')
+                    ->or_like('username', '%'.$q.'%')
+                    ->or_like('email', '%'.$q.'%');
 
-    public function find($id) {
-        return $this->db->table($this->table)->where($this->primary_key, $id)->get();
-    }
+                // Clone before pagination
+                $countQuery = clone $query;
 
-    public function insert($data) {
-        return $this->db->table($this->table)->insert($data);
-    }
+                $data['total_rows'] = $countQuery->select_count('*', 'count')
+                                                ->get()['count'];
 
-    public function update($id, $data) {
-        return $this->db->table($this->table)->where($this->primary_key, $id)->update($data);
-    }
+                $data['records'] = $query->pagination($records_per_page, $page)
+                                        ->get_all();
 
-    public function delete($id) {
-        return $this->db->table($this->table)->where($this->primary_key, $id)->delete();
+                return $data;
+            }
+        }
+
     }
-}
+?>
